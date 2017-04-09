95298f304cf9c55875ce5535887e9cfd1a83e75a
ThisIsDisruptor <a_s_bas@mail.ru>
2017-04-09 17:20:14 +0300

    Connection string is added

--- BusStation/Driver.cs
+++ BusStation/Driver.cs
@@ -28,5 +28,10 @@ namespace BusStation
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Waybill> Waybills { get; set; }
-    }
+
+		public override string ToString()
+		{
+			return String.Format($"{Passport}  {FullName}");
+		}
+	}
 }
